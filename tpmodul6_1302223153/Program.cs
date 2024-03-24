2874e9af0d08d512e5b67816a0aea0ab9d3387b1
rafliad <radenistri@gmail.com>
2024-03-24 16:10:35 +0700

    inisialisasi projek

---
+++ tpmodul6_1302223153/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tpmodul6_1302223153
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
